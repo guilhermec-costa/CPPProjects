29351baa1c7f5639643eb7a0c66994fcc0064bfa
Guilherme China <echina725@gmail.com>
2024-06-15 13:52:28 -0300

    user input

--- main.cpp
+++ main.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
+#include <ostream>
+#include <string>
 using namespace std;
 
+void variables();
+void inputText();
+
 int main() {
+  inputText();
+  return 0;
+}
+
+void variables() {
   /*
   endl: new line(\n) in the output stream buffer + flushes the output stream
   buffer
   */
-  cout << "hello" << flush;
+  int numberX = 3;
+  int numberY = 5;
+  string dogFirstname = "churros ";
+  string dogLastname = "augusto";
+  int *pNumberX = &numberX;
+  numberX += 2;
   cout << "Hello world!" << endl;
-  return 0;
+  cout << numberX + numberY << endl;
+  cout << *pNumberX << endl;
+  cout << dogFirstname + dogLastname << endl;
 }
 
-void outputText() { cout << "Output text" << endl; }
+void inputText() {
+  cout << "Tell me your name: " << flush;
+  string input1;
+  string input2;
+  cin >> input1;
+  cin >> input2;
+  cout << "Input 1: " << input1 << endl;
+  cout << "Input 2: " << input2 << endl;
+}
