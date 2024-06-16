c5a64bcf3ff95ca9d0b0883a21b5467e4110ac5d
Guilherme China <echina725@gmail.com>
2024-06-15 21:49:57 -0300

    stuff...

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <climits>
+#include <iomanip>
+#include <ios>
 #include <iostream>
 #include <ostream>
 #include <string>
@@ -5,9 +8,13 @@ using namespace std;
 
 void variables();
 void inputText();
+void integers();
+void floats();
+void booleans();
+void conditions();
 
 int main() {
-  inputText();
+  booleans();
   return 0;
 }
 
@@ -36,4 +43,31 @@ void inputText() {
   cin >> input2;
   cout << "Input 1: " << input1 << endl;
   cout << "Input 2: " << input2 << endl;
+}
+
+void integers() {
+  int value = 2147483647;
+  long longValue = 29464836438;
+  short int shortValue = 256;
+  cout << sizeof(value) << endl;
+  cout << sizeof(shortValue) << endl;
+  cout << INT_MAX << endl;
+}
+
+void floats() {
+  float value = 76.53856385355;
+  double dValue = 76.53856385355;
+  cout << setprecision(20) << fixed << value << endl;
+  cout << setprecision(20) << fixed << dValue << endl;
+}
+
+void booleans() {
+  bool condition1 = true;
+  bool condition2 = false;
+  char oneByte1 = '7';
+  cout << condition1 << endl;
+  cout << condition2 << endl;
+  cout << "------" << endl;
+  cout << (int)oneByte1 << endl;
+  cout << (char)55 << endl;
 }
